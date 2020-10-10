a3281bd92440023fdf025fe53bf2622479d812fe
Daniel Precioso <danipg1409@gmail.com>
2020-10-10 20:03:28 +0200

    Round to 2

--- src/nc_to_json.py
+++ src/nc_to_json.py
@@ -62,7 +62,7 @@ def convert_to_json(date: str,
     v_lon = nc_sub['uo'].data[0,:,:]
     v_lat = nc_sub['vo'].data[0,:,:]
     
-    data = np.append(v_lon, v_lat).tolist()
+    data = np.append(v_lon, v_lat).round(2).tolist()
     
     json_dict = {'header': header,
                 'data': data}
