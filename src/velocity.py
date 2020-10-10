33d51e7a729977cc62206e2b4762adb07665c87b
borjasanlei <borja.sanchez.leirado@gmail.com>
2020-10-10 17:33:14 +0200

    Inverse velocity function.

--- src/velocity.py
+++ src/velocity.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.interpolate import interp2d
 
 
-def interpolate_discrete(lat, long, grid_array, array_latitudes, array_longitudes, sector_interpolation=False,
+def interpolate_discrete(lat, long, grid_array, array_latitudes,
+                         array_longitudes, sector_interpolation=False,
                          step=0.01):
     """
     Given K 2D-arrays, representing some K-dim magnitude values, interpolates the K values for a given latitude and
@@ -52,16 +53,18 @@ def interpolate_discrete(lat, long, grid_array, array_latitudes, array_longitude
     interpolated_values = np.zeros([k_dimensions])
     if sector_interpolation:
         lat_s = np.arange(array_latitudes[idx], array_latitudes[idx + 1], step)
-        long_s = np.arange(array_longitudes[idx], array_longitudes[1][idx + 1], step)
+        long_s = np.arange(array_longitudes[idx], array_longitudes[1][idx + 1],
+                           step)
         interpolated_sector = np.zeros([len(lat_s), len(long_s), k_dimensions])
     for i, dimension in enumerate(np.arange(k_dimensions)):
-        interpolation_function = interp2d(small_lat, small_long, small_array[:, :, dimension])
+        interpolation_function = interp2d(small_lat, small_long,
+                                          small_array[:, :, dimension])
 
         # Interpolate value
         interpolated_values[i] = interpolation_function(lat, long)
 
         if sector_interpolation:
-            interpolated_sector[:,:,i] = interpolation_function(lat_s, long_s)
+            interpolated_sector[:, :, i] = interpolation_function(lat_s, long_s)
 
     if sector_interpolation:
         return interpolated_values, lat_s, long_s, interpolated_sector
@@ -69,7 +72,9 @@ def interpolate_discrete(lat, long, grid_array, array_latitudes, array_longitude
         return interpolated_values
 
 
-def velocity_composition(boat_velocity, lat, long, stream_velocities, stream_velocities_latitudes, stream_velocities_longitudes):
+def velocity_composition(boat_velocity, lat, long, stream_velocities,
+                         stream_velocities_latitudes,
+                         stream_velocities_longitudes):
     """
     Given streams velocities on a discrete grid of latitude and longitude points, and a determined boat velocity, it
     compounds the sum of both velocities, interpolating the stream velocity for the latitude and longitude
@@ -77,29 +82,36 @@ def velocity_composition(boat_velocity, lat, long, stream_velocities, stream_vel
 
     Params:
     ------
-    boat_velocity: array. Vector containing the latitude and longitude components of the boat velocity, in that order.
-    lat: float. Latitude.
-    long: float. Longitude.
-    grid_array: array. An (M, N, K)-dimensional array containing the K-dim values of the magnitude. Each MxN matrix
-        element represents the value for the k_th dimension of that magnitude in the m_th, n_th latitude, longitude point
-        on the discrete space.
-    array_latitudes: array. An (M)-dimensional vector with the latitude values corresponding to the discrete points of
-        the grid_array.
-    array_longitudes: array. An (N)-dimensional vector with the longitude values corresponding to the discrete points of
-        the grid_array.
+    - boat_velocity: array. Vector containing the latitude and longitude
+    - components of the boat velocity, in that order.
+    * lat: float. Latitude.
+    * long: float. Longitude.
+    * grid_array: array. An (M, N, K)-dimensional array containing the K-dim
+        values of the magnitude. Each MxN matrix
+        element represents the value for the k_th dimension of that magnitude in
+        the m_th, n_th latitude, longitude point on the discrete space.
+    * array_latitudes: array. An (M)-dimensional vector with the latitude
+        values corresponding to the discrete points of the grid_array.
+    * array_longitudes: array. An (N)-dimensional vector with the longitude
+        values corresponding to the discrete points of the grid_array.
 
     Returns:
     ------
-    velocity. array. A vector with the composed sum of velocities, with the latitude and longitude components on that
-        order.
+    * velocity. array. A vector with the composed sum of velocities, with the
+        latitude and longitude components on that order.
     """
-    interpolated_stream_velocities = interpolate_discrete(lat, long, stream_velocities, stream_velocities_latitudes, stream_velocities_longitudes)
+    interpolated_stream_velocities = interpolate_discrete(lat, long,
+                                                          stream_velocities,
+                                                          stream_velocities_latitudes,
+                                                          stream_velocities_longitudes)
     velocity = boat_velocity + interpolated_stream_velocities
 
     return velocity
 
 
-def boat_movement(boat_velocity, lat, long, stream_velocities, stream_velocities_latitudes, stream_velocities_longitudes, ts):
+def boat_movement(boat_velocity, lat, long, stream_velocities,
+                  stream_velocities_latitudes, stream_velocities_longitudes,
+                  ts):
     """
     Computes the new position of the boat given the boat velocity, the steams velocity and the timestamp. Timestamp
     is assumed to be so small that the stream velocity does not change from the initial point to the final one.
@@ -124,7 +136,81 @@ def boat_movement(boat_velocity, lat, long, stream_velocities, stream_velocities
 
     """
 
-    velocity = velocity_composition(boat_velocity, lat, long, stream_velocities, stream_velocities_latitudes, stream_velocities_longitudes)
+    velocity = velocity_composition(boat_velocity, lat, long, stream_velocities,
+                                    stream_velocities_latitudes,
+                                    stream_velocities_longitudes)
     new_latitude = lat + velocity[0] * ts
     new_longitude = lat + velocity[1] * ts
     return new_latitude, new_longitude
+
+
+def desired_velocity(initial_point, final_point,
+                     stream_velocities, stream_velocities_latitudes,
+                     stream_velocities_longitudes, ts, max_distance=1):
+    """Given the stream speeds, a couple of points, A and B, assuming that the
+    distance between those points is much smaller than the 'stream change
+    distance', dL, returns the boat velocity and time spent by the boat to
+    reach that point.
+
+    Params:
+    ------
+    initial_point: array. 1D vector, containing the initial point
+        coordinates, expressed as latitude and longitude values.
+    final_point: array. 1D vector, containing the destination point
+        coordinates, expressed as latitude and longitude values.
+    grid_array: array. An (M, N, K)-dimensional array containing the K-dim
+        values of the magnitude. Each MxN matrix element represents the value
+        for the k_th dimension of that magnitude in the m_th, n_th latitude,
+        longitude point on the discrete space.
+    array_latitudes: array. An (M)-dimensional vector with the latitude values
+        corresponding to the discrete points of the grid_array.
+    array_longitudes: array. An (N)-dimensional vector with the longitude values
+        corresponding to the discrete points of the grid_array.
+    time_spent: float. Time spent on that journey.
+    max_distance: float. Default = 1. Max distance that can be considered for
+        the uniform approximation. In units of grid resolution distance.
+
+    Returns:
+    ------
+
+    boat_velocity: array. 1D Vector containing the latitude and longitude
+        components of the boat velocity, in that order.
+    """
+
+    # Ensure distance is small enough for uniform regime
+    distance = np.sqrt(np.sum((final_point - initial_point) ** 2))
+
+    # Assuming that the grid is squared
+    grid_distance = np.abs(stream_velocities_latitudes[1]
+                           - stream_velocities_latitudes[0])
+
+    if distance >= max_distance * grid_distance:
+        print('That distance is huge for me!')
+        boat_velocity = np.zeros([2])
+
+    else:
+        lat = initial_point[0]
+        long = initial_point[1]
+
+        final_lat = final_point[0]
+        final_long = final_point[1]
+
+        stream_initial_velocity = interpolate_discrete(
+            lat, long,
+            stream_velocities,
+            stream_velocities_latitudes,
+            stream_velocities_longitudes)
+
+        stream_final_velocity = interpolate_discrete(
+            final_lat, final_long,
+            stream_velocities,
+            stream_velocities_latitudes,
+            stream_velocities_longitudes)
+
+        stream_mean_velocity = (stream_initial_velocity +
+                                stream_final_velocity) / 2
+
+        velocity = (final_point - initial_point) / ts
+        boat_velocity = velocity - stream_mean_velocity
+
+    return boat_velocity
